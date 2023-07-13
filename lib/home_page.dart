85d264512807b92ab82c6339003c596a90de6fab
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2023-07-13 11:17:54 +0530

    Done code

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text('Wellcome to home page!'),
+      ),
+    );
+  }
+}
